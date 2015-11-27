5b4c83a862168374db9b8b939c4d52cd7198e9a8
Robert Ochoa <ochoa.robert1@gmail.com>
2015-11-27 15:28:55 -0430

    sustracción de carga en el footer, da error fatal

--- functions.php
+++ functions.php
@@ -205,17 +205,6 @@ function proyecto_load_js() {
 
 add_action('init', 'proyecto_load_js');
 
-// Cargar en el footer los javascripts
-function footer_enqueue_scripts() {
-    remove_action('wp_head', 'wp_print_scripts');
-    remove_action('wp_head', 'wp_print_head_scripts', 9);
-    remove_action('wp_head', 'wp_enqueue_scripts', 1);
-    add_action('wp_footer', 'wp_print_scripts', 5);
-    add_action('wp_footer', 'wp_enqueue_scripts', 5);
-    add_action('wp_footer', 'wp_print_head_scripts', 5);
-}
-add_action('after_setup_theme', 'footer_enqueue_scripts');
-
 /* --------------------------------------------------------------
     ADD THEME SUPPORT
 -------------------------------------------------------------- */
